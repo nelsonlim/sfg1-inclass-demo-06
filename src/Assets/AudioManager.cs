5b715dc68fd758302a20f76bae627730b525bfcb
Nelson Lim <me@nelsonlim.com>
2020-02-27 19:52:48 -0600

    Initial commit

---
+++ src/Assets/AudioManager.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance = null;
+    public AudioClip bgMainMenuClip;
+    public AudioClip bgGameClip;
+
+    private AudioSource audioSrc;
+    private String currentLevel;
+
+    private void Awake()
+    {
+        CreateSingleton();
+        audioSrc = this.gameObject.GetComponent<AudioSource>();
+    }
+
+    private void CreateSingleton()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        } else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayBGMusic();
+        currentLevel = SceneManager.GetActiveScene().name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayNextLevelBGMusic();
+    }
+
+    private void PlayNextLevelBGMusic()
+    {
+        if (currentLevel != SceneManager.GetActiveScene().name)
+        {
+            currentLevel = SceneManager.GetActiveScene().name;
+            audioSrc.Stop();
+            audioSrc.clip = GetCurrentBGClip();
+            audioSrc.Play();
+        }
+    }
+
+    private void PlayBGMusic()
+    {
+        audioSrc.clip = GetCurrentBGClip();
+        if (audioSrc.isPlaying)
+        {
+            audioSrc.Stop();
+        }
+        audioSrc.Play();
+    }
+
+    private AudioClip GetCurrentBGClip()
+    {
+        String activeScene = SceneManager.GetActiveScene().name;
+        if (activeScene == "MainMenu")
+        {
+            return bgMainMenuClip;
+        } else
+        {
+            return bgGameClip;
+        }
+    }
+}
---
+++ src/Assets/LevelController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            LoadLevel("Level01");
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            LoadLevel("Level02");
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            LoadLevel("MainMenu");
+        }
+    }
+
+    private void LoadLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
